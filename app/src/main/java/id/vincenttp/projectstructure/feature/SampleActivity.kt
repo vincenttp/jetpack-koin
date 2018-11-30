fd20b024d5e28355389407da202962228d895167
vincenttp <tolanda.vincent>
2018-11-30 21:31:12 +0700

    implement app bundle and dynamic feature

--- app/src/main/java/id/vincenttp/projectstructure/feature/SampleActivity.kt
+++ app/src/main/java/id/vincenttp/projectstructure/feature/SampleActivity.kt
@@ -1,13 +1,23 @@
 package id.vincenttp.projectstructure.feature
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import id.vincenttp.projectstructure.R
 import id.vincenttp.projectstructure.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_sample.*
 import javax.inject.Inject
 
 class SampleActivity : BaseActivity(), SampleContract.SampleView {
     @Inject
     lateinit var presenter: SamplePresenter
+
+    private val splitInstallManager: SplitInstallManager by lazy {
+        SplitInstallManagerFactory.create(applicationContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +25,31 @@ class SampleActivity : BaseActivity(), SampleContract.SampleView {
 
         presenter.view = this
         presenter.getD()
+
+        button.setOnClickListener {
+            loadDynamicFeature()
+        }
+
+    }
+
+    private fun loadDynamicFeature() {
+        if (splitInstallManager.installedModules.contains("dynamic_feature")){
+            Intent().setClassName(packageName, "id.vincenttp.dynamic_feature.MainDynamicActivity")
+                    .also {
+                        startActivity(it)
+                    }
+        }
+
+        val request = SplitInstallRequest
+                .newBuilder()
+                .addModule("dynamic_feature")
+                .build()
+
+        splitInstallManager.startInstall(request)
+                .addOnCompleteListener {
+                }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "fail", Toast.LENGTH_SHORT).show()
+                }
     }
 }
---
+++ dynamic_feature/src/main/java/id/vincenttp/dynamic_feature/MainDynamicActivity.kt
@@ -0,0 +1,12 @@
+package id.vincenttp.dynamic_feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainDynamicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_dynamic)
+    }
+}
